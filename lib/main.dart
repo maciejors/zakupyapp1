a62824381725fd451d38975ac4bd9c4c8de3f269
maciejors <maciej.orslowski@gmail.com>
2022-06-25 16:18:25 +0200

    add AppInfo class to retrieve app version

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:zakupyapk/screens/home.dart';
 import 'package:zakupyapk/screens/settings.dart';
+import 'package:zakupyapk/utils/app_info.dart';
 import 'package:zakupyapk/utils/storage_manager.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -9,6 +10,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SM.setupStorage();
+  await AppInfo.initialise();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
