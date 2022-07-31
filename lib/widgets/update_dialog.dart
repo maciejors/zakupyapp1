cf48848340d522418eaa3a3f4899abff403b098d
maciejors <maciej.orslowski@gmail.com>
2022-07-31 12:17:00 +0200

    add url launcher to update dialog's download action

--- lib/widgets/update_dialog.dart
+++ lib/widgets/update_dialog.dart
@@ -7,7 +7,37 @@ import 'package:zakupyapk/utils/app_info.dart';
 class DownloadUpdateDialog extends StatelessWidget {
   final AppRelease latestRelease;
 
-  const DownloadUpdateDialog({Key? key, required this.latestRelease}) : super(key: key);
+  const DownloadUpdateDialog({Key? key, required this.latestRelease})
+      : super(key: key);
+
+  void copyDownloadUrl(BuildContext context) {
+    Navigator.of(context).pop();
+    Clipboard.setData(ClipboardData(text: latestRelease.downloadUrl));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Link do pobrania aktualizacji skopiowany do schowka.'),
+      ),
+    );
+  }
+
+  void downloadInBrowser(BuildContext context) async {
+    Navigator.of(context).pop();
+    bool success = await launchUrl(
+      Uri.parse(latestRelease.downloadUrl),
+      mode: LaunchMode.externalApplication,
+    );
+    if (!success) {
+      Clipboard.setData(ClipboardData(text: latestRelease.downloadUrl));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          duration: Duration(seconds: 8),
+          content: Text('Nie udało się rozpocząć pobierania. '
+              'Link do pobrania aktualizacji został skopiowany do schowka. '
+              'Aby ręcznie pobrać aktualizację, wklej go do przeglądarki'),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,19 +61,13 @@ class DownloadUpdateDialog extends StatelessWidget {
         ),
         TextButton(
           onPressed: () {
-            // commented out - doesn't work TODO
-            // launchUrl(Uri.parse(latestRelease.downloadUrl));
-            Clipboard.setData(
-                ClipboardData(text: latestRelease.downloadUrl));
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(
-                    'Link do pobrania aktualizacji skopiowany do '
-                        'schowka. Wklej go do przeglądarki'),
-              ),
-            );
-
-            Navigator.of(context).pop();
+            copyDownloadUrl(context);
+          },
+          child: Text('Skopiuj link'),
+        ),
+        TextButton(
+          onPressed: () {
+            downloadInBrowser(context);
           },
           child: Text('Pobierz'),
         ),
