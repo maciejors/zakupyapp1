fb144e569e4236b48234877ff7e6000bb2999580
maciejors <maciej.orslowski@gmail.com>
2023-09-07 11:47:53 +0200

    add trim and extra conditions when saving preferences

--- lib/storage/storage_manager.dart
+++ lib/storage/storage_manager.dart
@@ -15,7 +15,7 @@ class SM {
     // remove characters with a special meaning to the firebase
     shoppingListId = shoppingListId.replaceAll(RegExp(r'[/#\.\$\[\]]'), '');
 
-    _storage.setString('shoppingListId', shoppingListId);
+    _storage.setString('shoppingListId', shoppingListId.trim());
   }
 
   static double getMainFontSize() {
@@ -30,7 +30,27 @@ class SM {
     return _storage.getString('username') ?? 'Użytkownik bez nazwy';
   }
 
-  static void setUserName(String username) {
-    _storage.setString('username', username);
+  static void setUsername(String username) {
+    if (username == '') {
+      username = 'Użytkownik bez nazwy';
+    }
+    _storage.setString('username', username.trim());
+  }
+
+  static bool getCheckForUpdatesFlag() {
+    return _storage.getBool('checkForUpdatesFlag') ?? true;
+  }
+
+  static void setCheckForUpdatesFlag(bool checkForUpdatesFlag) {
+    _storage.setBool('checkForUpdatesFlag', checkForUpdatesFlag);
+  }
+
+  static bool getDisplayDeclaredProductsFlag() {
+    return _storage.getBool('displayDeclaredProductsFlag') ?? true;
+  }
+
+  static void setDisplayDeclaredProductsFlag(bool displayDeclaredProductsFlag) {
+    _storage.setBool(
+        'displayDeclaredProductsFlag', displayDeclaredProductsFlag);
   }
 }
