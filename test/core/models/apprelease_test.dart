1a207ead78fbcd3acdaad1a8ba1c17f278846ae6
maciejors <maciej.orslowski@gmail.com>
2023-09-20 19:00:42 +0200

    add 1 more getMaxVersion test

--- test/core/models/apprelease_test.dart
+++ test/core/models/apprelease_test.dart
@@ -2,15 +2,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:zakupyapp/core/models/apprelease.dart';
 
 void main() {
-  test('getMaxVersion() correctly returns the latest version ID', () {
-    var filenames = [
-      'zakupyapp-1.99.0.apk',
-      'zakupyapp-1.3.199.apk',
-      'zakupyapp-2.100.0.apk',
-      'zakupyapp-2.2.99.apk'
-    ];
-    String latest = '2.100.0';
-    expect(AppRelease.getMaxVersion(filenames), equals(latest));
+  group('getMaxVersion() correctly returns the latest version ID', () {
+    test('Case 1', () {
+      var filenames = [
+        'zakupyapp-1.99.0.apk',
+        'zakupyapp-1.3.199.apk',
+        'zakupyapp-2.100.0.apk',
+        'zakupyapp-2.2.99.apk',
+      ];
+      String latest = '2.100.0';
+      expect(AppRelease.getMaxVersion(filenames), equals(latest));
+    });
+    test('Case 2', () {
+      var filenames = [
+        'zakupyapp-1.5.2.apk',
+        'zakupyapp-1.5.0.apk',
+      ];
+      String latest = '1.5.2';
+      expect(AppRelease.getMaxVersion(filenames), equals(latest));
+    });
   });
 
   group('compareTo() correcly compares app versions', () {
